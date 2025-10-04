663dbc84b4881f9cbf71e30bf9b79d22af8c0d4b
Merlin Kreuzkam <miotares@proton.me>
2025-10-04 18:03:33 +0200

    Initial Commit

---
+++ BitcoinWidgets/BitcoinWidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinWidgetsApp.swift
+//  BitcoinWidgets
+//
+//  Created by Merlin Kreuzkam on 04.10.25.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinWidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
